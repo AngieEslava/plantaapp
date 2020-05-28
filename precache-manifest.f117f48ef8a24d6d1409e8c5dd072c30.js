e414a276ceb7d424522eed0fe40f735d2f5a7990
AngieEslava <egangiest@gmail.com>
2020-05-28 09:14:39 -0500

    Updates

---
+++ precache-manifest.f117f48ef8a24d6d1409e8c5dd072c30.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bb36f5bc62336315d25fc5692fe0f5d",
+    "url": "/plantaapp/index.html"
+  },
+  {
+    "revision": "13551d682f425aac2a91",
+    "url": "/plantaapp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "129dee909b070d332c5f",
+    "url": "/plantaapp/static/css/main.6b4532cf.chunk.css"
+  },
+  {
+    "revision": "13551d682f425aac2a91",
+    "url": "/plantaapp/static/js/2.e150fbdd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/plantaapp/static/js/2.e150fbdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "129dee909b070d332c5f",
+    "url": "/plantaapp/static/js/main.3fc681f0.chunk.js"
+  },
+  {
+    "revision": "abad2299eff388c42ff6",
+    "url": "/plantaapp/static/js/runtime-main.cef481c2.js"
+  },
+  {
+    "revision": "ba631b638722bb02c7a4ee1998ba6224",
+    "url": "/plantaapp/static/media/banner_img.ba631b63.jpg"
+  },
+  {
+    "revision": "0d3d721254ae1141a1533f413fcd768c",
+    "url": "/plantaapp/static/media/logo.0d3d7212.png"
+  }
+]);
